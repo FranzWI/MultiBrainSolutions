e091880f76593498ad14590b1ede1235a70a9fc8
mkuerbis <derdudele@gmail.com>
2014-09-25 14:16:40 +0200

    Lib für Elasticsearch hinzugefügt

---
+++ src/de/mbs/servlet/ElasticsearchServlet.java
@@ -0,0 +1,20 @@
+package de.mbs.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class ElasticsearchServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7913911436790432794L;
+
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+	@Override
+	public void destroy() {
+		
+	}
+	
+}
