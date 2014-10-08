86f6bc95eb72298a36c724ff8235ba264a446112
mkuerbis <michael.kuerbis@gmx.de>
2014-10-08 20:42:33 +0200

    Mail Versand angepasst

--- src/de/mbs/mail/sendgrid/SendGridView.java
+++ src/de/mbs/mail/sendgrid/SendGridView.java
@@ -7,23 +7,22 @@ import de.mbs.interfaces.MailView;
 
 public class SendGridView extends MailView {
 
-	
 	private SendGrid sendgrid;
-	
-	public SendGridView(){
-		sendgrid = new SendGrid(SGProp.USER,SGProp.PASSWORD);
+
+	public SendGridView() {
+		sendgrid = new SendGrid(SGProp.USER, SGProp.PASSWORD);
 	}
-	
+
 	@Override
 	public boolean sendMail(String to, String topic, String from, String text) {
 		SendGrid.Email email = new SendGrid.Email();
-	    email.addTo(to);
-	    email.setFrom(from);
-	    email.setSubject(topic);
-	    email.setText(text);
-	    try {
-			SendGrid.Response response =  sendgrid.send(email);
-			System.out.println("Email versandstatus: "+response.getMessage());
+		email.addTo(to);
+		email.setFrom(from);
+		email.setSubject(topic);
+		email.setText(text);
+		try {
+			SendGrid.Response response = sendgrid.send(email);
+			System.out.println("Email versandstatus: " + response.getMessage());
 			return response.getStatus();
 		} catch (SendGridException e) {
 			// TODO Auto-generated catch block
@@ -32,13 +31,14 @@ public class SendGridView extends MailView {
 		return false;
 	}
 
-	public String getServiceName(){
+	public String getServiceName() {
 		return "SendGrid";
 	}
-	
+
 	@Override
 	public boolean isRunning() {
-		return this.sendMail("derdudele@gmail.com", "testmail", "management-cockpit@ba-dresden.de", "testmail");
+		return this.sendMail("derdudele@gmail.com", "testmail",
+				"management-cockpit@ba-dresden.de", "testmail");
 	}
 
 }
