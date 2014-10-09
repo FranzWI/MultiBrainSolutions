7e58ca7f8e59c7feddaae4155efbbd19fac3d8ea
mkuerbis <michael.kuerbis@gmx.de>
2014-10-09 14:17:01 +0200

    Test

--- src/de/mbs/mail/sendgrid/SGProp.java
+++ src/de/mbs/mail/sendgrid/SGProp.java
@@ -3,6 +3,6 @@ package de.mbs.mail.sendgrid;
 public class SGProp {
 
 	protected final static String USER = "mkuerbis";
-	protected final static String PASSWORD = "=6J^4~hyDU@+N\\#";
+	protected final static String PASSWORD = "ngKFM}9C%&gPWBs";
 	
 }
