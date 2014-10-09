dd0284b64c5f3c9565edec962f051db1642f116f
mkuerbis <michael.kuerbis@gmx.de>
2014-10-09 14:11:08 +0200

    SG Prop untracked

--- src/de/mbs/mail/sendgrid/SGProp.java
+++ src/de/mbs/mail/sendgrid/SGProp.java
@@ -2,7 +2,7 @@ package de.mbs.mail.sendgrid;
 
 public class SGProp {
 
-	protected final static String USER = "";
-	protected final static String PASSWORD = "";
+	protected final static String USER = "mkuerbis";
+	protected final static String PASSWORD = "=6J^4~hyDU@+N\\#";
 	
 }
