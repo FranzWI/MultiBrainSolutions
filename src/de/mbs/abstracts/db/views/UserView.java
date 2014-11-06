a086b39a32125aff2daa9b40ba7446bbef0e0077
FranzWI <FranzMeinel@yahoo.de>
2014-11-06 13:24:21 +0100

    User -> ESCView version & ESC allgemein
    Hey Leute,
    
    ich habe heute mal angefangen ein bisschen was an der UserView zu machen
    und alles in allem ist es noch nicht so viel. Ich habe auch an der
    ElasticsearchView eine Änderung vorgenommen die für dich(Kürby)
    interessant sein könnte! Ich habe die getproperty von String[][] auf
    Object[][] umgewandelt .... Ist aber alles im quellcode kommentiert. Ich
    brauche jetzt von Toby & oder Kürby ein wenig Input, inwieweit ich dann
    an die bestehenden elasticsearch daten ran komme (Ich glaube ich brauche
    dazu die passenden DB Indexe!?

--- src/de/mbs/abstracts/db/views/UserView.java
+++ src/de/mbs/abstracts/db/views/UserView.java
@@ -1,5 +1,9 @@
 package de.mbs.abstracts.db.views;
 
+import groovy.xml.MarkupBuilder;
+
+import java.util.Vector;
+
 import de.mbs.abstracts.db.objects.User;
 import de.mbs.abstracts.db.views.definition.AddableView;
 import de.mbs.abstracts.db.views.definition.EditableView;
@@ -18,5 +22,5 @@ public abstract class UserView implements AddableView<User>,
 	 *         vorliegt ansonsten die ID des betroffenen Nutzers
 	 */
 	public abstract String login(String username, String password);
-
+		
 }
