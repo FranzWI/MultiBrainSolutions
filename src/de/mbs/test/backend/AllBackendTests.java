bf66ac3322d6fc0d02f31ea09c398e57957a005d
dcraig23 <nise23@gmail.com>
2014-12-15 16:05:06 +0100

    momentan nur userView Test ausführen

--- src/de/mbs/test/backend/AllBackendTests.java
+++ src/de/mbs/test/backend/AllBackendTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ UserviewTest.class, PortletviewTest.class, GroupviewTest.class })
+@SuiteClasses({ UserviewTest.class }) //hier alle zu testenden views einfügen
+//@SuiteClasses({ UserviewTest.class, PortletviewTest.class, GroupviewTest.class })
 public class AllBackendTests {
 }
