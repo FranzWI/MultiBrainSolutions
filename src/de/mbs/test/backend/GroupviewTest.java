4dc62ed191ef03a7b3bd5aadfe7dc15b35b682b3
Biaz89 <tobias-plotz>
2015-02-18 08:21:58 +0100

    GroupViewTests completed @mkuerbis IDs der AdminGroup und UserGroup überprüfen

--- src/de/mbs/test/backend/GroupviewTest.java
+++ src/de/mbs/test/backend/GroupviewTest.java
@@ -26,56 +26,57 @@ import de.mbs.test.TestExecuter;
 public class GroupviewTest {
 	
 	private static String groupID;
-	
-	/**
-	 * Test method for {@link de.mbs.abstracts.db.DatabaseView#getGroupView()}.
-	 */
-/*	@Test
-	public void test0IsGroupViewImplemented() {
-		GroupView groupView = TestExecuter.getView().getGroupView();
-		assertNotNull("Groupview nicht implementiert", groupView);
-	}*/
+	private static Group group;
 	
 	/**
 	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#JavaGroupview()}.
 	 */
-/*	@Test
-	public final void testJavaGroupview() {
-		fail("Not yet implemented"); // TODO
-	}*/
+	@Test
+	public final void test1JavaGroupview() {
+		GroupView groupView = TestExecuter.getView().getGroupView();
+		assertNotNull("Groupview nicht implementiert", groupView);
+	}
 
 	/**
 	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#add(de.mbs.abstracts.db.objects.Group)}.
 	 */
 	@Test
-	public final void testAdd() {
+	public final void test2Add() {
 		
 		GroupView groupView = TestExecuter.getView().getGroupView();
-		Group group = new Group(null);
+		group = new Group(null);
 		group.setName("TestGroup");
 		groupID = groupView.add(group);
 		assertNotNull("Group wurde nicht angelegt", groupID);
-		System.out.println("Original GroupID = "+groupID);
-		System.out.println("Original GroupName = "+group.getName());
-		
 	}
-
+	
+	/**
+	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#get(java.lang.String)}.
+	 */
+	@Test
+	public final void test3Get() {
+		GroupView groupView = TestExecuter.getView().getGroupView();
+		group = groupView.get(groupID);
+		
+//		System.out.println("Original GroupID = "+groupID);
+//		System.out.println("Original GroupName = "+group.getName());
+	}
 	/**
 	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#edit(de.mbs.abstracts.db.objects.Group)}.
 	 */
+	
 	@Test
-	public final void testEdit() {
+	public final void test4Edit() {
 		try {
 		GroupView groupView = TestExecuter.getView().getGroupView();
-		Group group = groupView.get(groupID);
 		group.setName("Edit");
 		group.setDescription("Edited group");
 		
 		groupView.edit(group);
 		assertNotNull("EditGroup ist Null", groupID);
 		assertTrue("Neuer Name ist nicht Edit", group.getName() == "Edit");
-		System.out.println("New GroupName = "+ group.getName());
-		System.out.println("New GroupDescription = "+ group.getDescription());
+//		System.out.println("New GroupName = "+ group.getName());
+//		System.out.println("New GroupDescription = "+ group.getDescription());
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -83,33 +84,27 @@ public class GroupviewTest {
 	}
 
 	/**
-	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#get(java.lang.String)}.
-	 */
-/*	@Test
-	public final void testGet() {
-		fail("Not yet implemented"); // TODO
-	}*/
-
-	/**
 	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#remove(java.lang.String)}.
 	 */
-/*	@Test
-	public final void testRemove() {
-		fail("Not yet implemented"); // TODO
-	}*/
+	@Test
+	public final void test5Remove() {
+		GroupView groupView = TestExecuter.getView().getGroupView();
+		groupView.remove(groupID);
+		assertNull("Gruppe existiert noch", groupView.get(groupID));
+	}
 
 	/**
 	 * Test method for {@link de.mbs.db.java.views.JavaGroupview#getAll()}.
 	 */
 	@Test
-	public final void testGetAll() {
+	public final void test6GetAll() {
 		try {
 		GroupView groupView = TestExecuter.getView().getGroupView();
 		Vector<Group> vec = groupView.getAll();
-		for (Group g:groupView.getAll())
-			{
-				System.out.println("GroupVector = "+g.getName());
-			}
+//		for (Group g:groupView.getAll())
+//			{
+//				System.out.println("GroupVector = "+g.getName());
+//			}
 		assertNotNull("GroupVector ist Null", vec);
 		assertTrue("GroupVector ist leer", vec.size() > 0);
 		}
@@ -123,18 +118,28 @@ public class GroupviewTest {
 	/**
 	 * Test method for {@link de.mbs.abstracts.db.views.GroupView#getAdminGroupId()}.
 	 */
-/*	@Test
+	@Test
 	public final void testGetAdminGroupId() {
-		fail("Not yet implemented"); // TODO
-	}*/
+		String adminGroupID;
+		
+		GroupView groupView = TestExecuter.getView().getGroupView();
+		adminGroupID = groupView.getAdminGroupId();
+		System.out.println("ID der AdminGroup = "+ adminGroupID);
+		assertNotNull("AdminGroupID ist null", adminGroupID);
+	}
 
 	/**
 	 * Test method for {@link de.mbs.abstracts.db.views.GroupView#getUserGroupId()}.
 	 */
-/*	@Test
+	@Test
 	public final void testGetUserGroupId() {
-		fail("Not yet implemented"); // TODO
-	}*/
+		String userGroupID;
+		
+		GroupView groupView = TestExecuter.getView().getGroupView();
+		userGroupID = groupView.getAdminGroupId();
+		System.out.println("ID der UserGroup = "+ userGroupID);
+		assertNotNull("AdminGroupID ist null", userGroupID);
+	}
 
 	/**
 	 * Test method for {@link de.mbs.abstracts.db.views.GroupView#update(java.util.Observable, java.lang.Object)}.
