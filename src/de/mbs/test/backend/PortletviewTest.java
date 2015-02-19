50bbc177c65dc138691f4be8f4c62e886600c974
dcraig23 <nise23@gmail.com>
2015-02-19 09:49:06 +0100

    Portletview zum Testen vorbereitet

--- src/de/mbs/test/backend/PortletviewTest.java
+++ src/de/mbs/test/backend/PortletviewTest.java
@@ -7,8 +7,38 @@ import org.junit.Test;
 public class PortletviewTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public final void testGetPossiblePortletsForUser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testJavaPortletview() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEdit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGet() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testRemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetAll() {
+		fail("Not yet implemented"); // TODO
 	}
 
 }
