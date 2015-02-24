5c64cc8987e03c28c7d94ff2b30b0219684d7dd7
Biaz89 <tobias-plotz>
2015-02-24 12:03:33 +0100

    PortletViewTest

--- src/de/mbs/test/backend/PortletviewTest.java
+++ src/de/mbs/test/backend/PortletviewTest.java
@@ -13,48 +13,37 @@ import de.mbs.abstracts.db.views.UserView;
 import de.mbs.test.TestExecuter;
 
 public class PortletviewTest {
-//for(User u:userView.getAll()){System.out.println(u.getUsername());}
-	
+	// for(User u:userView.getAll()){System.out.println(u.getUsername());}
 
 	@Test
 	public final void testGetAll() {
 		PortletView portletView = TestExecuter.getView().getPortletView();
 		Vector<Portlet> vec = portletView.getAll();
-		for (Portlet p:vec){System.out.println(p.getName());}
-		assertNotNull("",vec);
+		for (Portlet p : vec) {
+			System.out.println(p.getName());
+		}
+		assertNotNull("", vec);
 	}
-/*	
-@Test
-	public final void testGetPossiblePortletsForUser() {
-	PortletView portletView = TestExecuter.getView().getPortletView();
-	portletView.getPossiblePortletsForUser(UserviewTest.testUserId);
-	}
-	*/
-/*
-	@Test
-	public final void testJavaPortletview() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testAdd() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testEdit() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGet() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testRemove() {
-		fail("Not yet implemented"); // TODO
-	}
-*/
+	/*
+	 * @Test public final void testGetPossiblePortletsForUser() { PortletView
+	 * portletView = TestExecuter.getView().getPortletView();
+	 * portletView.getPossiblePortletsForUser(UserviewTest.testUserId); }
+	 */
+	/*
+	 * @Test public final void testJavaPortletview() {
+	 * fail("Not yet implemented"); // TODO }
+	 * 
+	 * @Test public final void testAdd() { fail("Not yet implemented"); // TODO
+	 * }
+	 * 
+	 * @Test public final void testEdit() { fail("Not yet implemented"); // TODO
+	 * }
+	 * 
+	 * @Test public final void testGet() { fail("Not yet implemented"); // TODO
+	 * }
+	 * 
+	 * @Test public final void testRemove() { fail("Not yet implemented"); //
+	 * TODO }
+	 */
 
 }
